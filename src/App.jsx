a8de3df9a09e14efdf9e39689d64011a3476bf71
datcord <theodorezisimopoulos@gmail.com>
2024-12-08 01:02:58 +0200

    created initial game table

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
-export default function Square() {
-  return <button className="square">X</button>;
+function Square({value}) {
+  return <button className="square">{value}</button>
 }
 
-  
+
+export default function Board() {
+  return(
+    <>
+      <div className="board-row">
+        <Square value={"x"}/>
+        <Square value={"x"}/>
+        <Square value={"x"}/>
+      </div>
+      <div className="board-row">
+        <button className="square"></button>
+        <button className="square"></button>
+        <button className="square"></button>
+      </div>
+      <div className="board-row">
+        <button className="square"></button>
+        <button className="square"></button>
+        <button className="square"></button>
+      </div>
+    </>
+  ) ;
+}
