1619cfbecb4e7202d98aab424a0b3477c273939d
datcord <theodorezisimopoulos@gmail.com>
2024-12-07 12:49:56 +0200

    first commit

---
+++ src/components/ListGroup.jsx
@@ -0,0 +1,14 @@
+function ListGroup() {
+  //let items = ["ny","fs","dsfs"];
+  return (
+    <>
+      <h1>List Group</h1>
+      <ul className="list-group">
+        {
+          
+        }
+      </ul>
+    </>
+  );
+}
+export default ListGroup;
